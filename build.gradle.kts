428b088d0b9d13dfd9f1f95a9c12a73c0888d56c
Sayyid A Naqvi <naqvi.sdsu@gmail.com>
2020-07-07 11:01:49 -0500

    fixed the release entry for 'projectreactor' in buil.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.3.1.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
-
-//    id("io.projectreactor.kotlin") version "1.0.2.RELEASE"      // didn't work
-
     kotlin("jvm") version "1.3.72"
     kotlin("plugin.spring") version "1.3.72"
 
@@ -25,10 +22,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-//    implementation 'io.projectreactor.kotlin:reactor-kotlin-extensions:1.0.1.RELEASE'
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.0.1.RELEASE")
 
-    // ompile "io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.0-M1"
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
